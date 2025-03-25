28bc202b1d38489b79be8ea578e8d05610ed06d4
LakshmiMarneedi34 <nagavaralakshmimarneedi5@gmail.com>
2025-03-25 19:40:23 +0530

    cra and tailwing configured

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-red-300 text-3xl underline'>
+        Hello!
+        <p className='font-bold'>Haii</p>
+        </div>
+    </>
+  )
+}
+
+export default App
